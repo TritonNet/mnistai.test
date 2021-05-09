1a9dd366e8fff5faacb9bad71efcf27b55cf4417
Kushan Fernando <kushan.fernando@gallagher.com>
2021-05-10 03:00:00 +1200

    initial files

---
+++ src/mnistai.cpu/mnistai.cpu.cpp
@@ -0,0 +1,10 @@
+// mnistai.cpu.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnmnistaicpu()
+{
+}
